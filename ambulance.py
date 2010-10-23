46dc7376464a039694b5e70042ebcb77257850c3
Max Sobell <max.sobell@cs.nyu.edu>
2010-10-22 22:41:08 -0400

    Added moving the ambulance. Untested

--- ambulance.py
+++ ambulance.py
@@ -10,6 +10,7 @@ from scipy.cluster.vq import vq, kmeans, whiten
 import time
 
 start_time = time.time()
+total_saved = 0
 
 def usage():
     sys.stdout.write( __doc__ % os.path.basename(sys.argv[0]))
@@ -19,6 +20,7 @@ class Ambulance:
         self.x = x
         self.y = y
         self.time = 0
+        self.cargo = []
 
     def move(self,destx,desty):
         self.time += abs(x - destx) + abs(y - desty)
@@ -32,6 +34,7 @@ class Patient:
         self.y = y
         self.ttl = ttl
         self.hospital = None
+        self.scored = 10000
 
     def __repr__(self):
         return (("Patient %s at %s, %s. %s to live") %\
@@ -41,12 +44,13 @@ class Patient:
     def __cmp__(self, p):
         return (abs(self.x - p.x) + abs(self.y - p.y))
 
-    def score(self,x,y,thres):
+    def score(self,x,y,thres,time):
         d = abs(x - self.x) + abs(y - self.y)
-        if d > thres:
-            return -1
+        if d > thres or time > ttl:
+            self.score = 10000
         else:
-            return ttl*d
+            self.score = (ttl-time)+d
+        return self.score
 
 class Hospital:
     def __init__(self,num,x,y):
@@ -118,11 +122,33 @@ if __name__ == "__main__":
             ambulances.append(Ambulance(h.x,h.y))
 
     for a in ambulances:
-        count = 0
-        for p in patient:
-            p.score(a.x,a.y,5) # threshold of 5 blocks
-        while count < 4:
-            
+        while len(a.cargo) < 4: # load each ambulance all the way
+            mini = 10000
+            low_patient = patient[0]
+            for p in patients:
+                p.score(a.x,a.y,5,a.time) # threshold of 5 blocks
+                if p.scored < mini:
+                    mini = p.scored
+                    low_patient = p
+            a.move(low_patient.x,low_patient.y) # move the ambulance to the person
+            patients.remove(low_patient) # remove the patient from the street
+            a.cargo.append(low_patient) # add him/her to the ambulance
+            a.time += 1 # takes 1 minute to load the patient
+        closest_hospital = h[0]
+        mini = 1000
+        for h in hospitals:
+            d = abs(h.x - a.x) + abs(h.y - a.y)
+            if d < mini:
+                mini = d
+                closest_hospital = h
+        a.move(h.x,h.y)
+        a.time += 1 # unload all patients
+        for r in a.cargo:
+            if r.ttl > a.time:
+                total_saved += 1
+                print "Saved %s at %s. %s saved." % (repr(r.num),repr(a.time),repr(total_saved))
+            else:
+                print "Lost %s at %s." % (repr(r.num),repr(a.time))
             
     print "Time : ", round(time.time() - start_time,2)
 
